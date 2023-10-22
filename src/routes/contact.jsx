2607a195a84f2440d557f6d01cb6df99a811280d
snaupdog <nihal.satish@gmail.com>
2023-10-22 20:37:56 +0530

    react router working kind of

---
+++ src/routes/contact.jsx
@@ -0,0 +1,24 @@
+
+
+import Navbar from '../components/navbar/navbar.jsx'
+
+import Footer from "../components/footer/footer.jsx";
+
+
+
+export default function Contact(){
+
+    return(
+        <>
+
+        <Navbar/>
+
+        <Footer/>
+
+
+        </>
+    )
+
+
+
+}
---
+++ src/routes/facilities.jsx
@@ -0,0 +1,22 @@
+
+
+import Navbar from '../components/navbar/navbar.jsx'
+
+
+
+
+export default function Facilities(){
+
+    return(
+        <>
+
+        <Navbar/>
+
+
+
+        </>
+    )
+
+
+
+}
