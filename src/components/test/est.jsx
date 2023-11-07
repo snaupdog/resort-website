67848d09a3e08f58a4849757bcea6c287c897656
snau <nihal.satish@gmail.com>
2023-11-07 16:25:03 +0530

    nowayjose

--- src/components/test/est.jsx
+++ src/components/test/est.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import "./test.css";
 
+import img1 from "../../assets/map.webp";
 export default function Est() {
   const typingContainer = {
     hidden: { opacity: 0 },
@@ -21,21 +22,49 @@ export default function Est() {
       },
     },
   };
-
   return (
     <div className="est-container">
-      <motion.h1
+      <img src={img1} alt="fsadf" className="est-img1" />
+
+      <motion.h4
+        className="oasis-text"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: -50 }}
+        transition={{
+          type: "tween",
+          duration: 0.5,
+          delay: 1.2,
+        }}
+      >
+        OASIS
+      </motion.h4>
+
+      <motion.h4
+        className="resort-text"
+        initial={{ opacity: 0, y: 200 }}
+        animate={{ opacity: 1, y: 80 }}
+        transition={{
+          type: "tween",
+          duration: 0.5,
+
+          delay: 1,
+        }}
+      >
+        RESORTS
+      </motion.h4>
+
+      <motion.h4
         className="text"
         variants={typingContainer}
         initial="hidden"
         animate="show"
       >
-        {Array.from("Oasis Resort").map((word, i) => (
+        {Array.from("where blah blah eats blah blah").map((word, i) => (
           <motion.span key={i} variants={typingText}>
             {word}
           </motion.span>
         ))}
-      </motion.h1>
+      </motion.h4>
     </div>
   );
 }
