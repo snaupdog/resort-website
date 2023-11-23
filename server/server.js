c9498e3057865a7a31c638b608004c405e473f87
snau <nihal.satish@gmail.com>
2023-11-23 08:40:38 +0530

    backend connected

---
+++ server/server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+var bodyParser = require("body-parser");
+
+const { MongoClient } = require("mongodb");
+
+const app = express();
+const port = 3001;
+
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+client.connect((err) => {
+  if (err) {
+    console.error("Error connecting to database", err);
+  } else {
+    console.log("Connected to MongoDB");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
+
+app.post("/formdata", async (req, res) => {
+  try {
+    const data = req.body;
+    const db = client.db("Oasis");
+    const collection = db.collection("Contact");
+    const result = await collection.insertOne(data);
+
+    if (result.acknowledged) {
+      res.status(201).json({
+        message: "form created successfully",
+      });
+    } else {
+      console.error("Failed to insert form into the database");
+      res.status(500).json({ error: "Failed to create form" });
+    }
+  } catch (err) {
+    console.error("Error creating form:", err);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
